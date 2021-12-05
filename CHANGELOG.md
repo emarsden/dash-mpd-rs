0f617fd276485b76bf05a098869d8386adec35f2
Eric Marsden <eric.marsden@foncsi.org>
2021-12-05 13:30:50 +0100

    Downloading: add support for muxing using vlc as a subprocess

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,12 +2,19 @@
 
 ## [0.2.0] - 2021-12-XX
 ### Changed
+- Added support for using ffmpeg or vlc as a subprocess for muxing, rather than ffmpeg's libav
+  library. This is more robust on certain invalid media streams and may be easier to build on
+  certain platforms. Support is gated by the "libav" feature.
+- When using libav (ffmpeg as a library), errors and informational messages from ffmpeg will be
+  logged at info level.
+- The `serviceLocation` attribute on `BaseURL` nodes is now public.
+- The `media` attribute on `SegmentTemplate` nodes is now an optional field (it was previously a
+  required field)
 - On platforms that support extended filesystem attributes, write the origin MPD URL to attribute
   `user.xdg.origin.url` on the output media file, using the `xattr` crate, unless the URL contains
   sensitive information such as a password. Likewise, write any MPD.ProgramInformation.{Title,
   Source, Copyright} information from the MPD manifest to attributes user.dublincore.{title, source,
   rights}.
-- The `serviceLocation` attribute on `BaseURL` nodes is now public.
 - Downloading: improve handling of transient HTTP errors.
 
 ## [0.1.0] - 2021-12-01
--- src/ffmpeg.rs
+++ src/ffmpeg.rs
@@ -4,19 +4,22 @@
 /// "libav.rs".
 
 
+use std::fs;
 use std::process::Command;
 use anyhow::{Result, anyhow};
 
 
-pub fn mux_audio_video(audio_path: &str, video_path: &str, output_path: &str) -> Result<()> {
+fn mux_audio_video_ffmpeg(audio_path: &str, video_path: &str, output_path: &str) -> Result<()> {
     let ffmpeg = Command::new("ffmpeg")
         .env_clear() 
         .args(["-hide_banner", "-nostats",
                "-loglevel", "error",  // or "warning", "info"
                "-y",  // overwrite output file if it exists
                "-i", audio_path,
-               "-i", video_path, 
+               "-i", video_path,
                "-c:v", "copy", "-c:a", "copy",
+               // select the mp4 muxer explicitly (output_path doesn't necessarily have a .mp4 extension)
+               "-f", "mp4",
                output_path])
         .output()
         .expect("couldn't run ffmpeg subprocess");
@@ -25,6 +28,44 @@ pub fn mux_audio_video(audio_path: &str, video_path: &str, output_path: &str) ->
     } else {
         let msg = String::from_utf8(ffmpeg.stderr)?;
         Err(anyhow!("Failure running ffmpeg: {}", msg))
+    }
+}
+
+
+// See https://wiki.videolan.org/Transcode/
+fn mux_audio_video_vlc(audio_path: &str, video_path: &str, output_path: &str) -> Result<()> {
+    let vlc = Command::new("vlc")
+        .env_clear()
+        .args(["--no-repeat", "--no-loop", "-I", "dummy",
+               audio_path, video_path,
+               "--sout-keep",
+               &format!("--sout=#gather:transcode{{{}}}:standard{{access=file,mux=mp4,dst={}}}",
+                       "vcodec=h264,vb=1024,scale=1,acodec=mp4a",
+                       output_path),
+               "vlc://quit"])
+        .output()
+        .expect("couldn't run vlc subprocess");
+    if vlc.status.success() {
+        Ok(())
+    } else {
+        let msg = String::from_utf8(vlc.stderr)?;
+        Err(anyhow!("Failure running vlc: {}", msg))
+    }
+}
+
+
+// First try ffmpeg subprocess, if that fails try vlc subprocess
+pub fn mux_audio_video(audio_path: &str, video_path: &str, output_path: &str) -> Result<()> {
+    eprintln!("Muxing audio {}, video {}", audio_path, video_path);
+    if let Err(e) = mux_audio_video_ffmpeg(audio_path, video_path, output_path) {
+        log::info!("Muxing with ffmpeg subprocess failed: {}", e);
+        log::info!("Retrying mux with vlc subprocess");
+        if let Err(_) = fs::remove_file(output_path) {
+            // ffmpeg mux attempt didn't create any output
+        }
+        mux_audio_video_vlc(audio_path, video_path, output_path)
+    } else {
+        Ok(())
     }
 }
 
