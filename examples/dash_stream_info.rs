b6d62c709babdd85a74c718636dc8067a53b867d
Eric Marsden <eric.marsden@risk-engineering.org>
2022-11-05 10:12:02 +0100

    Fix example for updated field type

--- examples/dash_stream_info.rs
+++ examples/dash_stream_info.rs
@@ -42,37 +42,33 @@ fn main() {
     // Show audio tracks with the codecs and available bitrates. Note that MPD manifests used by
     // commercial streaming services often include separate audio tracks for the different dubbed
     // languages available, but audio may also be included with the video track.
-    if let Some(audio_adaptation) = period.adaptations.as_ref().and_then(|a| a.iter().find(is_audio_adaptation)) {
-        if let Some(reps) = &audio_adaptation.representations {
-            for representation in reps.iter() {
-                // Here we see some of the fun of dealing with the MPD format: the codec can be
-                // specified on the <Representation> element, or on the relevant <AdaptationSet>
-                // element, or not at all.
-                let codec = representation.codecs.as_ref()
-                    .unwrap_or_else(|| audio_adaptation.codecs.as_ref()
-                                    .unwrap_or(&unspecified));
-                let bw = match representation.bandwidth {
-                    Some(b) => b.to_string(),
-                    None => "<unspecified>".to_string(),
-                };
-                println!("Audio stream with codec {}, bandwidth {}", codec, bw);
-            }
+    if let Some(audio_adaptation) = period.adaptations.iter().find(is_audio_adaptation) {
+        for representation in audio_adaptation.representations.iter() {
+            // Here we see some of the fun of dealing with the MPD format: the codec can be
+            // specified on the <Representation> element, or on the relevant <AdaptationSet>
+            // element, or not at all.
+            let codec = representation.codecs.as_ref()
+                .unwrap_or_else(|| audio_adaptation.codecs.as_ref()
+                                .unwrap_or(&unspecified));
+            let bw = match representation.bandwidth {
+                Some(b) => b.to_string(),
+                None => "<unspecified>".to_string(),
+            };
+            println!("Audio stream with codec {}, bandwidth {}", codec, bw);
         }
     }
 
     // Show video tracks with the codecs and available bitrates
-    if let Some(video_adaptation) = period.adaptations.as_ref().and_then(|a| a.iter().find(is_video_adaptation)) {
-        if let Some(reps) = &video_adaptation.representations {
-            for representation in reps.iter() {
-                let codec = representation.codecs.as_ref()
-                    .unwrap_or_else(|| video_adaptation.codecs.as_ref()
-                                    .unwrap_or(&unspecified));
-                let bw = match representation.bandwidth {
-                    Some(b) => b.to_string(),
-                    None => "<unspecified>".to_string(),
-                };
-                println!("Video stream with codec {}, bandwidth {}", codec, bw);
-            }
+    if let Some(video_adaptation) = period.adaptations.iter().find(is_video_adaptation) {
+        for representation in video_adaptation.representations.iter() {
+            let codec = representation.codecs.as_ref()
+                .unwrap_or_else(|| video_adaptation.codecs.as_ref()
+                                .unwrap_or(&unspecified));
+            let bw = match representation.bandwidth {
+                Some(b) => b.to_string(),
+                None => "<unspecified>".to_string(),
+            };
+            println!("Video stream with codec {}, bandwidth {}", codec, bw);
         }
     }
 }
