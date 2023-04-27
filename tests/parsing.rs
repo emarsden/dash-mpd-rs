fee8031f1c1453229653a1de707aff4cb663319c
sbuzzard <steve_buzzard@cable.comcast.com>
2023-04-27 11:01:55 -0400

    Fallback to iso8603 if rfc3339 doesn't work; validate receipt of nanos

--- tests/parsing.rs
+++ tests/parsing.rs
@@ -12,21 +12,21 @@ use std::time::Duration;
 #[test]
 fn test_mpd_parser () {
     use dash_mpd::parse;
- 
+
     let case1 = r#"<?xml version="1.0" encoding="UTF-8"?><MPD><Period></Period></MPD>"#;
     let res = parse(&case1);
     assert!(res.is_ok());
     let mpd = res.unwrap();
     assert_eq!(mpd.periods.len(), 1);
     assert!(mpd.ProgramInformation.is_none());
-    
+
     let case2 = r#"<?xml version="1.0" encoding="UTF-8"?><MPD foo="foo"><Period></Period><foo></foo></MPD>"#;
     let res = parse(case2);
     assert!(res.is_ok());
     let mpd = res.unwrap();
     assert_eq!(mpd.periods.len(), 1);
     assert!(mpd.ProgramInformation.is_none());
-    
+
     let case3 = r#"<?xml version="1.0" encoding="UTF-8"?><MPD><Period></PeriodZ></MPD>"#;
     let res = parse(case3);
     assert!(res.is_err());
@@ -68,7 +68,7 @@ fn test_mpd_parser () {
 fn test_datetime_parsing () {
     use dash_mpd::parse;
     use chrono::{Timelike, Datelike};
-    
+
     let case1 = r#"<MPD minBufferTime="PT1.500S"></MPD>"#;
     let res = parse(case1);
     assert!(res.is_ok());
@@ -109,9 +109,18 @@ fn test_datetime_parsing () {
     assert_eq!(ast.day(), 3);
     assert_eq!(ast.minute(), 56);
 
-    // an invalid xs:datetime (month number 14)
-    let case5 = r#"<MPD availabilityStartTime="1066-14-03T21:56"></MPD>"#;
+    // an xs:datetime with a timezone specified parses fractional nanoseconds
+    let case5 = r#"<MPD availabilityStartTime="2021-06-03T13:00:00.543343989Z"></MPD>"#;
     let res = parse(case5);
+    assert!(res.is_ok());
+    let mpd = res.unwrap();
+    assert!(mpd.availabilityStartTime.is_some());
+    let ast = mpd.availabilityStartTime.unwrap();
+    assert_eq!(ast.nanosecond(), 543343989);
+
+    // an invalid xs:datetime (month number 14)
+    let case6 = r#"<MPD availabilityStartTime="1066-14-03T21:56"></MPD>"#;
+    let res = parse(case6);
     assert!(res.is_err());
 }
 
