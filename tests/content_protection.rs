6565b42c8b972f95b2236c2527c4c5b3f7cad17d
Eric Marsden <eric.marsden@risk-engineering.org>
2023-11-08 20:52:29 +0100

    Tests: allow for unpredictable ordering of media streams from shaka packager

--- tests/content_protection.rs
+++ tests/content_protection.rs
@@ -99,13 +99,17 @@ async fn test_decryption_webm() {
     check_file_size_approx(&out, 1_331_284);
     let meta = ffprobe(out.clone()).unwrap();
     assert_eq!(meta.streams.len(), 2);
-    let stream = &meta.streams[0];
-    assert_eq!(stream.codec_type, Some(String::from("audio")));
-    assert_eq!(stream.codec_name, Some(String::from("opus")));
-    let stream = &meta.streams[1];
-    assert_eq!(stream.codec_type, Some(String::from("video")));
-    assert_eq!(stream.codec_name, Some(String::from("vp9")));
-    assert!(stream.width.is_some());
+    // The order of audio and video streams in the output WebM container is unreliable with Shaka
+    // packager, so we need to test this carefully.
+    let audio = meta.streams.iter()
+        .find(|s| s.codec_type.eq(&Some(String::from("audio"))))
+        .expect("finding audio stream");
+    assert_eq!(audio.codec_name, Some(String::from("opus")));
+    let video = meta.streams.iter()
+        .find(|s| s.codec_type.eq(&Some(String::from("video"))))
+        .expect("finding video stream");
+    assert_eq!(video.codec_name, Some(String::from("vp9")));
+    assert!(video.width.is_some());
     let ffmpeg = Command::new("ffmpeg")
         .args(["-v", "error",
                "-i", &out.to_string_lossy(),
