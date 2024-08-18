510730f31db4e6e57c2f79d8eb5b2634a9182b1f
Eric Marsden <eric.marsden@risk-engineering.org>
2024-08-18 10:15:05 +0200

    Additional test for language=en_surround filter

--- tests/language.rs
+++ tests/language.rs
@@ -81,3 +81,33 @@ async fn test_subtitle_lang_stpp_im1t() {
     assert!((3598.0 < duration) && (duration < 3599.0));
     let _ = fs::remove_file(outpath);
 }
+
+
+// This manifest contains 3 audio streams, with lang=en_stereo, no-voices_stereo, en_surround. Ask
+// for the en_surround audio track (which has a larger bitrate) and check that the resulting file
+// size is plausible.
+#[test(tokio::test)]
+async fn test_lang_en_surroud() {
+    let mpd_url = "https://bitmovin-a.akamaihd.net/content/sintel/sintel.mpd";
+    let tmpd = tempfile::tempdir().unwrap();
+    let out = tmpd.path().join("sintel-surround.mp4");
+    DashDownloader::new(mpd_url)
+        .worst_quality()
+        .without_content_type_checks()
+        .audio_only()
+        .download_to(out.clone()).await
+        .unwrap();
+    check_file_size_approx(&out, 35_709_031);
+    let format = FileFormat::from_file(out.clone()).unwrap();
+    assert_eq!(format, FileFormat::Mpeg4Part14Video);
+    let meta = ffprobe(out.clone()).unwrap();
+    assert_eq!(meta.streams.len(), 1);
+    let audio = meta.streams.iter()
+        .find(|s| s.codec_type.eq(&Some(String::from("audio"))))
+        .expect("finding audio stream");
+    assert_eq!(audio.codec_name, Some(String::from("aac")));
+    let entries = fs::read_dir(tmpd.path()).unwrap();
+    let count = entries.count();
+    assert_eq!(count, 1, "Expecting a single output file, got {count}");
+    let _ = fs::remove_dir_all(tmpd);
+}
